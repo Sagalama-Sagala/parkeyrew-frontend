50ca03bd21ab851c61a4651340bab16c66bc7f9b
Bourbxn <jakkapat97@gmail.com>
2023-09-25 04:15:55 +0700

    refactor login page

--- src/assets/login_register/index.js
+++ src/assets/login_register/index.js
@@ -1,4 +1,6 @@
 import logo1 from "./logo1.png";
 import logo2 from "./logo2.png";
+import eye from "./eye.svg";
+import eye_off from "./eye_off.svg";
 
-export { logo1, logo2 };
+export { logo1, logo2, eye, eye_off };

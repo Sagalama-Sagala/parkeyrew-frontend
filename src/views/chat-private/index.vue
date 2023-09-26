1b085dced73fd561e8034aafcacb24541de5264d
Bourbxn <jakkapat97@gmail.com>
2023-09-27 04:14:31 +0700

    connect websocket

--- src/views/chat-private/index.vue
+++ src/views/chat-private/index.vue
@@ -2,13 +2,43 @@
   <Container
     ><div>
       {{ this.$route.params.id }}
+      <p>State: {{ connected }}</p>
+    </div>
+    <div v-for="(item, index) in messages" :key="item">
+      {{ item }}
     </div>
   </Container>
 </template>
 
 <script>
 import Container from "@/components/ChatContainer/index.vue";
+import { state, socket } from "@/socket";
+import { ref } from "vue";
+
 export default {
+  setup() {
+    const messages = ref([]);
+
+    socket.on("message", (message) => {
+      messages.value = message;
+      console.log(messages);
+    });
+
+    return { messages };
+  },
+  computed: {
+    connected() {
+      return state.connected;
+    },
+  },
+  methods: {
+    connect() {
+      socket.connect();
+    },
+    disconnect() {
+      socket.disconnect();
+    },
+  },
   components: {
     Container,
   },

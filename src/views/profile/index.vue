742aeecdd4000d09fba36204730c1c8e2d327a99
Kanyarat Chaiyanboon <kanyaratchaiyanboon527@gmail.com>
2023-09-27 23:17:08 +0700

    changing in profile page

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -6,73 +6,63 @@
             aria-label="Sidebar"
         >
             <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
-                <ul class="space-y-2 font-medium">
-                    <li>
-                        <a
-                            class="flex items-center p-2 text-gray-900 rounded-lg group cursor-pointer"
+                <ul class="space-y-2 font-medium text-gray-400">
+                    <li v-for="e in navs">
+                        <div
+                            @click="
+                                e.childs.length !== 0
+                                    ? (e.expand = !e.expand)
+                                    : (focusPage = e.page)
+                            "
+                            :class="`${
+                                e.childs.length !== 0
+                                    ? e.childs?.findIndex(
+                                          (child) => child.page === focusPage
+                                      ) !== -1
+                                        ? 'text-gray-800'
+                                        : ''
+                                    : focusPage === e.page
+                                    ? 'text-gray-800'
+                                    : ''
+                            }`"
+                            class="flex items-center p-2 rounded-lg hover:bg-gray-100 group cursor-pointer"
                         >
-                            <img class="w-6 h-6" :src="account" />
+                            <img class="w-6 h-6" :src="e.icon" />
                             <span class="flex-1 ml-3 whitespace-nowrap">
-                                บัญชีของฉัน
+                                {{ e.title }}
                             </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-                        >
-                            <span class="flex-1 ml-12 whitespace-nowrap"
-                                >ประวัติ</span
-                            >
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-                        >
-                            <span class="flex-1 ml-12 whitespace-nowrap"
-                                >ที่อยู่</span
-                            >
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-                        >
-                            <span class="flex-1 ml-12 whitespace-nowrap"
-                                >เปลี่ยนรหัสผ่าน</span
-                            >
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-                            ><img class="w-6 h-6" :src="cart" />
-                            <span class="flex-1 ml-3 whitespace-nowrap">
-                                การซื้อของฉัน
-                            </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-                            ><img class="w-6 h-6" :src="cart2" />
-                            <span class="flex-1 ml-3 whitespace-nowrap"
-                                >การขายของฉัน
-                            </span>
-                        </a>
+                        </div>
+
+                        <ul v-if="e.expand">
+                            <li v-for="sub in e?.childs">
+                                <div
+                                    @click="focusPage = sub.page"
+                                    :class="`${
+                                        focusPage === sub.page
+                                            ? 'text-gray-800'
+                                            : ''
+                                    }`"
+                                    class="flex items-center p-2 rounded-lg hover:bg-gray-100 group cursor-pointer"
+                                >
+                                    <span
+                                        class="flex-1 ml-12 whitespace-nowrap"
+                                        >{{ sub.title }}</span
+                                    >
+                                </div>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </div>
         </aside>
 
         <div class="p-4 sm:ml-64 h-screen bg-gray-400 mt-16">
-            ขก.ละ ขอยาดนอน
+            focusPage {{ focusPage }}
+            <div v-if="focusPage === 'prawat'">ประวัติ</div>
+            <div v-else-if="focusPage === 'teeyoo'">ที่อยู่</div>
+            <div v-else-if="focusPage === 'chagepass'">เปลี่ยนรหัสผ่าน</div>
+            <div v-else-if="focusPage === 'mybuy'">การซื้อของฉัน</div>
+            <div v-else-if="focusPage === 'mysell'">การขายของฉัน</div>
         </div>
     </div>
 </template>
@@ -83,9 +73,49 @@ export default {
     methods: {},
     data() {
         return {
+            focusPage: "prawat",
             account,
             cart,
             cart2,
+            navs: [
+                {
+                    title: "บัญชีของฉัน",
+                    icon: account,
+                    expand: true,
+                    page: null,
+                    childs: [
+                        {
+                            title: "ประวัติ",
+                            icon: account,
+                            page: "prawat",
+                        },
+                        {
+                            title: "ที่อยู่",
+                            icon: account,
+                            page: "teeyoo",
+                        },
+                        {
+                            title: "เปลี่ยนรหัสผ่าน",
+                            icon: account,
+                            page: "chagepass",
+                        },
+                    ],
+                },
+                {
+                    title: "การซื้อของฉัน",
+                    icon: cart,
+                    expand: false,
+                    childs: [],
+                    page: "mybuy",
+                },
+                {
+                    title: "การขายของฉัน",
+                    icon: cart2,
+                    expand: false,
+                    childs: [],
+                    page: "mysell",
+                },
+            ],
         };
     },
 };

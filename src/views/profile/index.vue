74dcc0c841e9b645f3bda1d180e9d29841d0af84
Bourbxn <jakkapat97@gmail.com>
2023-09-28 01:28:24 +0700

    chat connect backend

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,70 +1,64 @@
 <template>
-    <div class="bg-gray-200 pt-[171px]">
-        <aside
-            id="cta-button-sidebar"
-            class="fixed left-0 h-screen transition-transform -translate-x-full sm:translate-x-0"
-            aria-label="Sidebar"
-        >
-            <div class="h-full p-16 overflow-y-auto text-xl">
-                <ul class="space-y-2 font-medium text-gray-400">
-                    <li v-for="e in navs">
-                        <div
-                            @click="
-                                e.childs.length !== 0
-                                    ? (e.expand = !e.expand)
-                                    : (focusPage = e.page)
-                            "
-                            :class="`${
-                                e.childs.length !== 0
-                                    ? e.childs?.findIndex(
-                                          (child) => child.page === focusPage
-                                      ) !== -1
-                                        ? 'text-gray-800'
-                                        : ''
-                                    : focusPage === e.page
-                                    ? 'text-gray-800'
-                                    : ''
-                            }`"
-                            class="flex items-center p-2 rounded-lg hover:bg-gray-100 group cursor-pointer"
-                        >
-                            <img class="w-6 h-6" :src="e.icon" />
-                            <span class="flex-1 ml-3 whitespace-nowrap">
-                                {{ e.title }}
-                            </span>
-                        </div>
-
-                        <ul v-if="e.expand">
-                            <li v-for="sub in e?.childs">
-                                <div
-                                    @click="focusPage = sub.page"
-                                    :class="`${
-                                        focusPage === sub.page
-                                            ? 'text-gray-800'
-                                            : ''
-                                    }`"
-                                    class="flex items-center p-2 rounded-lg hover:bg-gray-100 group cursor-pointer"
-                                >
-                                    <span
-                                        class="flex-1 ml-12 whitespace-nowrap"
-                                        >{{ sub.title }}</span
-                                    >
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+  <div class="bg-gray-200 pt-[171px]">
+    <aside
+      id="cta-button-sidebar"
+      class="fixed left-0 h-screen transition-transform -translate-x-full sm:translate-x-0"
+      aria-label="Sidebar"
+    >
+      <div class="h-full p-16 overflow-y-auto text-xl">
+        <ul class="space-y-2 font-medium text-gray-400">
+          <li v-for="e in navs">
+            <div
+              @click="
+                e.childs.length !== 0
+                  ? (e.expand = !e.expand)
+                  : (focusPage = e.page)
+              "
+              :class="`${
+                e.childs.length !== 0
+                  ? e.childs?.findIndex((child) => child.page === focusPage) !==
+                    -1
+                    ? 'text-gray-800'
+                    : ''
+                  : focusPage === e.page
+                  ? 'text-gray-800'
+                  : ''
+              }`"
+              class="flex items-center p-2 rounded-lg hover:bg-gray-100 group cursor-pointer"
+            >
+              <img class="w-6 h-6" :src="e.icon" />
+              <span class="flex-1 ml-3 whitespace-nowrap">
+                {{ e.title }}
+              </span>
             </div>
-        </aside>
 
-        <div class="p-4 ml-80 h-screen">
-            <!-- focusPage {{ focusPage }} -->
-            <div v-if="focusPage === 'record'"><Myrecord /></div>
-            <div v-else-if="focusPage === 'address'"><Address /></div>
-            <div v-else-if="focusPage === 'chagepass'"><Changepass /></div>
-            <div v-else-if="focusPage === 'mybuy'"><Mybuy /></div>
-            <div v-else-if="focusPage === 'mysell'"><Mysell /></div>
-        </div>
+            <ul v-if="e.expand">
+              <li v-for="sub in e?.childs">
+                <div
+                  @click="focusPage = sub.page"
+                  :class="`${focusPage === sub.page ? 'text-gray-800' : ''}`"
+                  class="flex items-center p-2 rounded-lg hover:bg-gray-100 group cursor-pointer"
+                >
+                  <span class="flex-1 ml-12 whitespace-nowrap">{{
+                    sub.title
+                  }}</span>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </aside>
+
+    <div class="p-4 ml-80 h-screen">
+      <!-- focusPage {{ focusPage }} -->
+      <div v-if="focusPage === 'record'"><Myrecord /></div>
+      <div v-else-if="focusPage === 'address'"><Address /></div>
+      <div v-else-if="focusPage === 'chagepass'"><Changepass /></div>
+      <div v-else-if="focusPage === 'mybuy'"><Mybuy /></div>
+      <div v-else-if="focusPage === 'mysell'"><Mysell /></div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -75,61 +69,60 @@ import Mysell from "@/components/Profile/Mysell/index.vue";
 import Myrecord from "@/components/Profile/Myrecord/index.vue";
 import { account, cart, cart2 } from "@/assets/profile";
 export default {
-    components: {
-        Address,
-        Changepass,
-        Mybuy,
-        Mysell,
-        Myrecord,
-    },
-    methods: {},
-    data() {
-        return {
-            focusPage: "record", //default
-            account,
-            cart,
-            cart2,
-            navs: [
-                {
-                    title: "บัญชีของฉัน",
-                    icon: account,
-                    expand: true,
-                    page: null,
-                    childs: [
-                        {
-                            title: "ประวัติ",
-                            icon: account,
-                            page: "record",
-                        },
-                        {
-                            title: "ที่อยู่",
-                            icon: account,
-                            page: "address",
-                        },
-                        {
-                            title: "เปลี่ยนรหัสผ่าน",
-                            icon: account,
-                            page: "chagepass",
-                        },
-                    ],
-                },
-                {
-                    title: "การซื้อของฉัน",
-                    icon: cart,
-                    expand: false,
-                    childs: [],
-                    page: "mybuy",
-                },
-                {
-                    title: "การขายของฉัน",
-                    icon: cart2,
-                    expand: false,
-                    childs: [],
-                    page: "mysell",
-                },
-            ],
-        };
-    },
+  components: {
+    Address,
+    Changepass,
+    Mybuy,
+    Mysell,
+    Myrecord,
+  },
+  methods: {},
+  data() {
+    return {
+      focusPage: "record", //default
+      account,
+      cart,
+      cart2,
+      navs: [
+        {
+          title: "บัญชีของฉัน",
+          icon: account,
+          expand: true,
+          page: null,
+          childs: [
+            {
+              title: "ประวัติ",
+              icon: account,
+              page: "record",
+            },
+            {
+              title: "ที่อยู่",
+              icon: account,
+              page: "address",
+            },
+            {
+              title: "เปลี่ยนรหัสผ่าน",
+              icon: account,
+              page: "chagepass",
+            },
+          ],
+        },
+        {
+          title: "การซื้อของฉัน",
+          icon: cart,
+          expand: false,
+          childs: [],
+          page: "mybuy",
+        },
+        {
+          title: "การขายของฉัน",
+          icon: cart2,
+          expand: false,
+          childs: [],
+          page: "mysell",
+        },
+      ],
+    };
+  },
 };
 </script>
-

678f602039095edaceeab0e4426d983a6f14a403
กฤตยชญ์ แสงงาม <hon@Macintosh.local>
2023-09-27 23:39:24 +0700

    deploy

---
+++ src/assets/product/index.js
@@ -0,0 +1,5 @@
+import shareArrow from "./share-arrow.svg";
+import heart from "./heart.svg";
+import chat from "./chat.svg";
+import call from "./call.svg";
+export { shareArrow, heart, chat, call };

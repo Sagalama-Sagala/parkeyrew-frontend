b4ae5b0baccb80b67c6b3c34cc3bec3cac30df91
mmayyiisuay <natchasuaysaard@gmail.com>
2023-10-05 22:40:07 +0700

    mockup address page update

--- src/views/profile/account/address/index.vue
+++ src/views/profile/account/address/index.vue
@@ -3,10 +3,42 @@
     <div class="md:text-2xl text-lg my-2 font-semibold">ที่อยู่ของฉัน</div>
     <button
       class="border-[0.05rem] border-primary px-4 py-2 rounded-lg text-primary"
+      @click="togglePopup"
     >
       เพิ่ม +
     </button>
   </div>
+  <div v-if="showPopup" class="z-20 h-screen w-full fixed left-0 top-0 bg-black bg-opacity-30" @click="togglePopup"></div>
+  <div
+      class="z-30 fixed top-[20%] right-[25%] h-[500px] items-center px-8 bg-white py-5 rounded w-[600px] shadow-xl"
+      :class="showPopup ? 'block' : 'hidden'"
+    >
+  <div class="">
+    <p class="text-2xl font-bold">ที่อยู่ใหม่</p>
+    <div class="flex justify-between mt-4">
+      <input
+              type="text"
+              class="focus:outline-none mt-1 block w-[48%] px-2 py-2 bg-white border border-black rounded-md text-sm shadow-sm placeholder-slate-400 font-normal"
+              placeholder="ชื่อจริง"
+            />  
+      <input
+              type="text"
+              class="focus:outline-none mt-1 block w-[48%] px-2 py-2 bg-white border border-black rounded-md text-sm shadow-sm placeholder-slate-400 font-normal"
+              placeholder="นามสกุล"
+            />  
+    </div>
+    <textarea
+              type="text"
+              class="focus:outline-none block w-full h-[100px] mt-4 px-2 py-2 bg-white border border-black rounded-md text-sm shadow-sm placeholder-slate-400 font-normal"
+              placeholder="บ้านเลขที่,ซอย,หมู่,ถนน,แขวง/ตำบล"
+            />  
+
+  </div>
+
+  
+  
+  </div>
+
   <div class="border-b-[0.08rem] border-black mr-12 my-8 w-full"></div>
   <div
     v-for="(adr, index) in address"
@@ -16,12 +48,12 @@
     <div class="w-[50%] my-4 pl-4">
       <div class="flex justify-around">
         <p class="w-1/6">{{ adr.name }}</p>
-        <p class="w-1/4">{{ adr.lastname }}</p>
+        <p class="w-1/4 ">{{ adr.lastname }}</p>
         <div class="w-[10px] border-r-[0.08rem] h-[24px] border-black"></div>
         <p class="w-1/4">{{ adr.phone }}</p>
       </div>
       <div class="w-full font-normal">
-        <p class="break-words">
+        <p class="break-words mt-2">
           {{ adr.add }}
         </p>
       </div>
@@ -50,31 +82,39 @@
     </div>
 
   </div>
+  
 
   
 </template>
 
 <script>
 export default {
+  
   data() {
     return {
+      showPopup: false,
       address: [
         {
           name: "ณัชชา",
           lastname: "สวยสะอาด",
           phone: "0949193350",
-          add: "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdassdasdasdasdasdasdasdasdasdasdasd",
+          add: "A3 - 409 อาคาร AJ park 701 ซอยฉลองกรุง 1 ถนนฉลองกรุง แขวง ลาดกระบัง เขตลาดกระบัง 10520",
           priority: 1,
         },
         {
           name: "ณัชชา",
           lastname: "สวยสะอาด",
           phone: "0949193350",
-          add: "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdassdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdassdasdasdasdasdasdasdasdasdasdasd",
+          add: "247 หมู่ 7 บ้านโน้น ตำบลนี้ อำเถอนู้น จังหวัดนั้นนั้น 33000",
           priority: 0,
         },
       ],
     };
   },
+    methods: {
+      togglePopup() {
+        this.showPopup = !this.showPopup;
+      },
+    },
 };
 </script>
--- src/views/profile/account/record/index.vue
+++ src/views/profile/account/record/index.vue
@@ -1,77 +1,77 @@
 <template>
-    
-  <div class="md:text-2xl text-lg">
-      <div class="md:text-2xl text-lg my-2 font-semibold">ข้อมูลของฉัน</div>
-      <div class="md:text-lg text-sm my-2">
-          จัดการข้อมูลส่วนตัวคุณเพื่อความปลอดภัยของบัญชี้ผู้ใช้นี้
-      </div>
-  </div>
-  <div class="border-b-[0.08rem] border-black mr-12 my-8 w-full"></div>
-  <div class="md:text-2xl text-lg flex md:flex-row flex-col-reverse md:items-start items-center">
-      <div v-if="pageState === 0" class="md:w-1/2 w-full md:ml-0 ml-16 md:mt-0 mt-[62px]">
-          <Defaultrecord/>
-      </div>
-      <div v-if="pageState === 1" class="md:w-1/2 w-full md:ml-0 ml-16">
-          <Edit/>
-      </div>
-      <div class="w-1/2 flex flex-col items-center">
-          <img class="md:w-36 md:h-36 w-28 h-28 md:mt-8" :src="view" />
-          <button v-if="pageState === 1"
-              class="border-[0.05rem] border-black px-2 rounded-lg my-4"
-          >
-              เลือกรูป
-          </button>
-          <div v-if="pageState === 1" class="text-xs mb-2">
-            <p>ขนาดไฟล์: สูงสุด 1 MB</p>
-            <p>ไฟล์ที่รองรับ: .JPEG, .PNG</p>
-          </div>
-      </div>
-  </div>
-  <div class="flex md:mt-10 justify-center">
-      <div v-if="pageState === 1" class="flex flex-col">
-          <button
-              class="my-2  border-2 border-[#204E8F] font-semibold text-[#204E8F] py-2 px-6 rounded-lg"
-              @click="save"
-          >
-              บันทึก
-          </button>
-      </div>
-      <button
-          v-if="pageState === 0"
-          class="my-2 border-2 border-[#204E8F] font-semibold text-[#204E8F] py-2 px-6 rounded-lg"
-          @click="edit"
-      >
-          แก้ไข
-      </button>
-  </div>
-  <div class="border-b-[0.08rem] border-black mr-12 my-8 w-full"></div>
+        
+    <div class="md:text-2xl text-lg">
+        <div class="md:text-2xl text-lg my-2 font-semibold">ข้อมูลของฉัน</div>
+        <div class="md:text-lg text-sm my-2">
+            จัดการข้อมูลส่วนตัวคุณเพื่อความปลอดภัยของบัญชี้ผู้ใช้นี้
+        </div>
+    </div>
+    <div class="border-b-[0.08rem] border-black mr-12 my-8 w-full"></div>
+    <div class="md:text-2xl text-lg flex md:flex-row flex-col-reverse md:items-start items-center">
+        <div v-if="pageState === 0" class="md:w-1/2 w-full md:ml-0 ml-16 md:mt-0 mt-[62px]">
+            <Defaultrecord/>
+        </div>
+        <div v-if="pageState === 1" class="md:w-1/2 w-full md:ml-0 ml-16">
+            <Edit/>
+        </div>
+        <div class="w-1/2 flex flex-col items-center">
+            <img class="md:w-36 md:h-36 w-28 h-28 md:mt-8" :src="view" />
+            <button v-if="pageState === 1"
+                class="border-[0.05rem] border-black px-2 rounded-lg my-4"
+            >
+                เลือกรูป
+            </button>
+            <div v-if="pageState === 1" class="text-xs mb-2">
+                <p>ขนาดไฟล์: สูงสุด 1 MB</p>
+                <p>ไฟล์ที่รองรับ: .JPEG, .PNG</p>
+            </div>
+        </div>
+    </div>
+    <div class="flex md:mt-10 justify-center">
+        <div v-if="pageState === 1" class="flex flex-col">
+            <button
+                class="my-2  border-2 border-[#204E8F] font-semibold text-[#204E8F] py-2 px-6 rounded-lg"
+                @click="save"
+            >
+                บันทึก
+            </button>
+        </div>
+        <button
+            v-if="pageState === 0"
+            class="my-2 border-2 border-[#204E8F] font-semibold text-[#204E8F] py-2 px-6 rounded-lg"
+            @click="edit"
+        >
+            แก้ไข
+        </button>
+    </div>
+    <div class="border-b-[0.08rem] border-black mr-12 my-8 w-full"></div>
 </template>
 
 <script>
-import { view } from "@/assets/profile";
-import Defaultrecord from "@/components/StepsEditRecord/Default/index.vue";
-import Edit from "@/components/StepsEditRecord/Edit/index.vue";
+    import { view } from "@/assets/profile";
+    import Defaultrecord from "@/components/StepsEditRecord/Default/index.vue";
+    import Edit from "@/components/StepsEditRecord/Edit/index.vue";
 
-export default {
-  components: {
-  Defaultrecord,
-  Edit,
-  },
-  data() {
-      return {
-          view,
-          pageState: 0,
-      };
-  },
-  methods: {
-      edit() {
-          this.pageState = 1;
-      },
-      save() {
-          this.pageState = 0;
-      },
-      
-  },
-};
+    export default {
+                components: {
+                Defaultrecord,
+                Edit,
+                },
+            data() {
+                return {
+                    view,
+                    pageState: 0,
+                };
+            },
+            methods: {
+                edit() {
+                    this.pageState = 1;
+                },
+                save() {
+                    this.pageState = 0;
+                },
+                
+            },
+        };
 </script>
 

e66ed482a229229ab19fc4c45baa7f90e9fbc095
Kanyarat Chaiyanboon <kanyaratchaiyanboon527@gmail.com>
2023-10-13 18:04:41 +0700

    update profile page

--- src/config/axios.js
+++ src/config/axios.js
@@ -4,4 +4,4 @@ import { getLocal } from "@/common/js/utils.js";
 axios.defaults.baseURL = import.meta.env.VITE_APP_API;
 axios.defaults.headers.common["Authorization"] = `Bearer ${getLocal("token")}`;
 axios.defaults.headers.post["Content-Type"] =
-  "application/x-www-form-urlencoded";
+    "application/x-www-form-urlencoded";

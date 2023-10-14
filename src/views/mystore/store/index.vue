337244b60cc003611b051a00ce013cbcd0da2350
Bourbxn <jakkapat97@gmail.com>
2023-10-14 14:27:45 +0700

    fetch data in my store page

--- src/views/mystore/store/index.vue
+++ src/views/mystore/store/index.vue
@@ -1,69 +1,124 @@
 <template>
   <div class="min-h-screen flex flex-col min-w-full">
-    <div class="bg-primary text-white flex flex-col items-center justify-center w-full mt-2 md:h-[36rem] h-[48rem] md:pt-0 pt-8">
-      <div class="bg-secondary text-black flex flex-col md:gap-6 gap-16 items-center justify-center md:w-[48rem] w-[16rem] md:h-[24rem] h-full mt-12 md:pb-0 pb-8 rounded-[3rem] shadow-[15.0px_15.0px_0.0px_rgba(0,0,0,0.18)] text-lg">
-        <div class="flex md:flex-row flex-col justify-between items-center md:w-[36rem] h-[4rem] md:pt-0 pt-8">
+    <div
+      class="bg-primary text-white flex flex-col items-center justify-center w-full mt-2 md:h-[36rem] h-[48rem] md:pt-0 pt-8"
+    >
+      <div
+        class="bg-secondary text-black flex flex-col md:gap-6 gap-16 items-center justify-center md:w-[48rem] w-[16rem] md:h-[24rem] h-full mt-12 md:pb-0 pb-8 rounded-[3rem] shadow-[15.0px_15.0px_0.0px_rgba(0,0,0,0.18)] text-lg"
+      >
+        <div
+          class="flex md:flex-row flex-col justify-between items-center md:w-[36rem] h-[4rem] md:pt-0 pt-8"
+        >
           <div>
-            <img class="h-[7rem] rounded-full mb-4" :src=profileURL alt="profile picture">
+            <img
+              class="h-[7rem] rounded-full mb-4"
+              :src="profileURL"
+              alt="profile picture"
+            />
           </div>
           <div class="flex flex-col justify-center w-[12rem] md:pt-0 pt-4">
             <div>
-              <b>{{ username }}</b>
+              <b>{{ profile?.username }}</b>
             </div>
             <div class="md:pt-0 pt-3">
-              <Rating :rating="reviewStar"/>
+              <Rating :rating="profile?.reviewStar" />
             </div>
           </div>
           <div class="flex flex-col">
             <div>
-              <img class="w-[3rem] rounded-2xl mb-2 hover:cursor-pointer md:block hidden" :src=editIcon alt="edit profile icon">
+              <img
+                class="w-[3rem] rounded-2xl mb-2 hover:cursor-pointer md:block hidden"
+                :src="editIcon"
+                alt="edit profile icon"
+              />
             </div>
             <div>
-              <img class="w-[3rem] rounded-2xl mb-3 hover:cursor-pointer md:block hidden" :src=shareIcon alt="share icon">
+              <img
+                class="w-[3rem] rounded-2xl mb-3 hover:cursor-pointer md:block hidden"
+                :src="shareIcon"
+                alt="share icon"
+              />
             </div>
           </div>
         </div>
-        <div class="md:text-lg text-sm flex flex-row justify-center items-center md:w-[36rem] md:h-[4rem] h-[2rem] md:border-b-2 md:border-t-2 border-black md:gap-10 gap-5 md:pb-6 md:pt-6 pt-[8.5rem]">
+        <div
+          class="md:text-lg text-sm flex flex-row justify-center items-center md:w-[36rem] md:h-[4rem] h-[2rem] md:border-b-2 md:border-t-2 border-black md:gap-10 gap-5 md:pb-6 md:pt-6 pt-[8.5rem]"
+        >
           <div class="hover:cursor-pointer" @click="openFollower">
-            <b>{{ follower }}  ผู้ติดตาม</b>
+            <b>{{ profile?.follower?.length }} ผู้ติดตาม</b>
           </div>
-          <Dialog v-if="followerDialog" @close="closeFollower" title="ผู้ติดตาม">
+          <Dialog
+            v-if="followerDialog"
+            @close="closeFollower"
+            title="ผู้ติดตาม"
+          >
             TEST Following Dialog
           </Dialog>
           |
           <div class="hover:cursor-pointer" @click="openFollowing">
-            <b>{{ following }}  กำลังติดตาม</b>
+            <b>{{ profile?.following?.length }} กำลังติดตาม</b>
           </div>
-          <Dialog v-if="followingDialog" @close="closeFollowing" title="กำลังติดตาม"/>
+          <Dialog
+            v-if="followingDialog"
+            @close="closeFollowing"
+            title="กำลังติดตาม"
+          />
         </div>
-        <div class="flex flex-col md:text-lg text-sm md:w-[32rem] w-[12rem] md:h-[4rem] h-[8rem] mb-8 pb-12">
+        <div
+          class="flex flex-col md:text-lg text-sm md:w-[32rem] w-[12rem] md:h-[4rem] h-[8rem] mb-8 pb-12"
+        >
           <b>คำอธิบาย</b>
-          <div>{{ description }}</div>
+          <div>{{ profile?.description }}</div>
         </div>
       </div>
-      <div class="flex md:flex-row flex-col gap-5 text-black text-xl items-center justify-center md:space-x-5 md:w-[46rem] mt-6 pb-6">
-        <div class="bg-tertiary flex items-center justify-center md:w-[22rem] w-[14rem] h-12 rounded-[1rem] hover:cursor-pointer" @click="routeTomyStore()"><h1><b>ร้านค้า</b></h1></div>
-        <div class="bg-secondary flex items-center justify-center md:w-[22rem] w-[14rem] h-12 rounded-[1rem] hover:cursor-pointer" @click="routeToReview()"><h1><b>รีวิว</b></h1></div>
+      <div
+        class="flex md:flex-row flex-col gap-5 text-black text-xl items-center justify-center md:space-x-5 md:w-[46rem] mt-6 pb-6"
+      >
+        <div
+          class="bg-tertiary flex items-center justify-center md:w-[22rem] w-[14rem] h-12 rounded-[1rem] hover:cursor-pointer"
+          @click="routeTomyStore()"
+        >
+          <h1><b>ร้านค้า</b></h1>
+        </div>
+        <div
+          class="bg-secondary flex items-center justify-center md:w-[22rem] w-[14rem] h-12 rounded-[1rem] hover:cursor-pointer"
+          @click="routeToReview()"
+        >
+          <h1><b>รีวิว</b></h1>
+        </div>
       </div>
     </div>
-    <div class="bg-secondary text-black flex flex-col flex-1 h-full items-center">
-      <div @click="handleToggle" class="text-primary border-primary active:text-secondary active:bg-primary flex items-center justify-center w-40 h-8 mt-5 text-lg border-2 rounded-md hover:cursor-pointer">
+    <div
+      class="bg-secondary text-black flex flex-col flex-1 h-full items-center"
+    >
+      <div
+        @click="handleToggle"
+        class="text-primary border-primary active:text-secondary active:bg-primary flex items-center justify-center w-40 h-8 mt-5 text-lg border-2 rounded-md hover:cursor-pointer"
+      >
         <h1>+ ลงขายสินค้า</h1>
       </div>
-      <div class="bg-secondary flex-1 flex flex-col w-full pt-6 overflow-y-auto">
-        <div class="flex md:flex-row md:flex-wrap flex-col w-full gap-5 justify-start md:pl-[5.2rem]">
+      <div
+        class="bg-secondary flex-1 flex flex-col w-full pt-6 overflow-y-auto"
+      >
+        <div
+          class="flex md:flex-row md:flex-wrap flex-col w-full gap-5 justify-start md:pl-[5.2rem]"
+        >
           <ProductCard
-            v-for="(item, index) in products"
-            :id="item.id"
-            :key="item.title"
+            v-for="(item, index) in profile.products"
+            :id="item._id"
+            :key="item.name"
             :is-recommended="item.recommended"
-            :item-name="item.title"
-            :tags="item.tags"
+            :item-name="item.name"
             :item-price="item.price"
             :item-image="item.productImage"
-            :rating="item.rating"
+            :rating="item.owner?.reviewStar"
             :seller-image="item.sellerImage"
+            :seller-name="item.owner?.username"
             :liked="item.liked"
+            :color="item.color"
+            :size="item.size"
+            :brand="item.brand"
+            :condition="item.condition"
           />
         </div>
       </div>
@@ -76,15 +131,30 @@
   </div>
 </template>
 
-<script >
+<script>
 import ProductCard from "@/components/ProductCard/index.vue";
 import PopupForm from "@/components/ProductInfo/PopupForm/index.vue";
 import Rating from "@/components/Rating/index.vue";
 import Dialog from "@/components/Dialog/index.vue";
-import {editIcon, shareIcon } from "@/assets/mystore";
-import axios from 'axios';
+import { editIcon, shareIcon } from "@/assets/mystore";
+import axios from "axios";
+import { ref } from "vue";
 
-export default{
+export default {
+  setup() {
+    const profile = ref({});
+    axios
+      .get("/user/get-user-page-by-id")
+      .then((response) => {
+        console.log(response.data);
+        profile.value = response.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return { profile };
+  },
   components: {
     ProductCard,
     Rating,
@@ -92,19 +162,21 @@ export default{
     PopupForm,
   },
 
-  data(){
-    return{
-      isModalOpen:false,
+  data() {
+    return {
+      isModalOpen: false,
       followerDialog: false,
       followingDialog: false,
       editIcon,
       shareIcon,
-      profileURL : "https://cdn.discordapp.com/attachments/968217024440455258/1161369443323093004/Cat.jpg?ex=65380c94&is=65259794&hm=aa9ff31c401b4cb5e6c9bb1a64478eafb111b0f00735dc487627d8f288c222d0&",
-      username : "HARIBO Goldbears",
-      reviewStar : 4,
-      follower : 29,
-      following : 9,
-      description : "สวัสดีครับ ท่านสมาชิกชมรมคนชอบ🐻 วันพระวันเจ้าไม่เว้นกันเลยอยากจะดูแต่🐻 ไม่เข้าใจจริงๆเลยทั้งเด็กทั้งผู้ใหญ่ตะโกนหาสรรหาแต่🐻 เป็นอะไรกัน! เฮ้ยย ",
+      profileURL:
+        "https://cdn.discordapp.com/attachments/968217024440455258/1161369443323093004/Cat.jpg?ex=65380c94&is=65259794&hm=aa9ff31c401b4cb5e6c9bb1a64478eafb111b0f00735dc487627d8f288c222d0&",
+      username: "HARIBO Goldbears",
+      reviewStar: 4,
+      follower: 29,
+      following: 9,
+      description:
+        "สวัสดีครับ ท่านสมาชิกชมรมคนชอบ🐻 วันพระวันเจ้าไม่เว้นกันเลยอยากจะดูแต่🐻 ไม่เข้าใจจริงๆเลยทั้งเด็กทั้งผู้ใหญ่ตะโกนหาสรรหาแต่🐻 เป็นอะไรกัน! เฮ้ยย ",
       products: [
         {
           id: "a0000001",
@@ -200,70 +272,65 @@ export default{
             { id: 2, label: "ลดได้อีกๆ" },
           ],
         },
-        
       ],
-    }
+    };
   },
-  methods : {
-    routeTomyStore(){
-      this.$router.push(`/mystore/store`)
+  methods: {
+    routeTomyStore() {
+      this.$router.push(`/mystore/store`);
     },
-    routeToReview(){
-      this.$router.push(`/mystore/review`)
+    routeToReview() {
+      this.$router.push(`/mystore/review`);
     },
-    openFollower(){
+    openFollower() {
       this.followerDialog = true;
     },
-    closeFollower(){
+    closeFollower() {
       this.followerDialog = false;
     },
-    openFollowing(){
+    openFollowing() {
       this.followingDialog = true;
     },
-    closeFollowing(){
+    closeFollowing() {
       this.followingDialog = false;
     },
-    handleToggle()
-    {
+    handleToggle() {
       this.isModalOpen = !this.isModalOpen;
     },
-    handleOk(value,resetData)
-    {
-        const newData =
-      {
-          "name": value.name,
-          "price": value.price,
-          "deliveryFee": value.deliveryFee,
-          "description": value.description,
-          "brand": value.brand,
-          "color": value.color,
-          "size": value.size,
-          "category": value.category,
-          "condition": value.condition,
-          "sendFrom": value.sendFrom,
-          "remain": value.remain,
-        }
-        axios.post('product/create-product', newData, {
-        headers: { Authorization : "Bearer " + `${localStorage.getItem('token')}`,  }
+    handleOk(value, resetData) {
+      const newData = {
+        name: value.name,
+        price: value.price,
+        deliveryFee: value.deliveryFee,
+        description: value.description,
+        brand: value.brand,
+        color: value.color,
+        size: value.size,
+        category: value.category,
+        condition: value.condition,
+        sendFrom: value.sendFrom,
+        remain: value.remain,
+      };
+      axios
+        .post("product/create-product", newData, {
+          headers: {
+            Authorization: "Bearer " + `${localStorage.getItem("token")}`,
+          },
         })
-      .then((response) => {
-        console.log(response.data);
-        resetData();
-        this.isModalOpen = false;
-      })
-      .catch((err) => {
-        console.log(err.response.data.message);
-        err.response.data.message.forEach(item=>
-        {
-          alert(item)
-        }
-        )
-      });
-      
+        .then((response) => {
+          console.log(response.data);
+          resetData();
+          this.isModalOpen = false;
+        })
+        .catch((err) => {
+          console.log(err.response.data.message);
+          err.response.data.message.forEach((item) => {
+            alert(item);
+          });
+        });
+
       //console.log(infoProducts)
-      
-    }
-    
-  }
-}
+    },
+  },
+};
 </script>

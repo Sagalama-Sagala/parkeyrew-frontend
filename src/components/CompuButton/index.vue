de0d4046e302435f1475be9113660db531fce7ea
Bourbxn <jakkapat97@gmail.com>
2023-10-17 19:31:53 +0700

    updat chompu button

--- src/components/CompuButton/index.vue
+++ src/components/CompuButton/index.vue
@@ -1,25 +1,24 @@
 <template>
-    <button :class="class" @click="onClick">
-        <img v-if="loading" class="animate-spin h-5 w-5" :src="loadingSVG" />
-        <p v-else>{{ text }}</p>
-    </button>
+  <button :class="class" @click="onClick" :disable="true">
+    <img v-if="loading" class="animate-spin h-5 w-5" :src="loadingSVG" />
+    <p v-else>{{ text }}</p>
+  </button>
 </template>
 
 <script>
 import { loadingSVG } from "@/assets/buttons";
 export default {
-    props: {
-        loading: false,
-        class: "",
-        text: "",
-        onClick: {
-            type: Function,
-        },
+  props: {
+    loading: false,
+    class: "",
+    text: "",
+    onClick: {
+      type: Function,
     },
-    setup() {},
-    data() {
-        return { loadingSVG };
-    },
+  },
+  setup() {},
+  data() {
+    return { loadingSVG };
+  },
 };
 </script>
-
--- src/views/profile/account/record/index.vue
+++ src/views/profile/account/record/index.vue
@@ -1,74 +1,72 @@
 <template>
-    <div class="md:text-2xl text-lg">
-        <div class="md:text-2xl text-lg my-2 font-semibold">ข้อมูลของฉัน</div>
-        <div class="md:text-lg text-sm my-2">
-            จัดการข้อมูลส่วนตัวคุณเพื่อความปลอดภัยของบัญชี้ผู้ใช้นี้
-        </div>
+  <div class="md:text-2xl text-lg">
+    <div class="md:text-2xl text-lg my-2 font-semibold">ข้อมูลของฉัน</div>
+    <div class="md:text-lg text-sm my-2">
+      จัดการข้อมูลส่วนตัวคุณเพื่อความปลอดภัยของบัญชี้ผู้ใช้นี้
     </div>
-    <div class="border-b-[0.08rem] border-black mr-12 my-8 w-full"></div>
+  </div>
+  <div class="border-b-[0.08rem] border-black mr-12 my-8 w-full"></div>
+  <div
+    class="md:text-2xl text-lg flex md:flex-row flex-col-reverse md:items-start items-center"
+  >
     <div
-        class="md:text-2xl text-lg flex md:flex-row flex-col-reverse md:items-start items-center"
+      v-if="pageState === 0"
+      class="md:w-1/2 w-full md:ml-0 ml-16 md:mt-0 mt-[62px]"
     >
-        <div
-            v-if="pageState === 0"
-            class="md:w-1/2 w-full md:ml-0 ml-16 md:mt-0 mt-[62px]"
-        >
-            <Defaultrecord />
-        </div>
-        <div v-if="pageState === 1" class="md:w-1/2 w-full md:ml-0 ml-16">
-            <Edit />
-        </div>
-        <div class="w-1/2 flex flex-col items-center">
-            <img
-                class="md:w-40 md:h-40 w-28 h-28 md:mt-8 rounded-full"
-                :src="
-                    imagePreview || profileStore.profile?.profileImage || view
-                "
-            />
-            <button
-                v-if="pageState === 1"
-                class="border-[0.05rem] border-black px-2 rounded-lg my-4"
-                @click="$refs.file.click()"
-            >
-                เลือกรูป
-            </button>
-            <input
-                type="file"
-                hidden
-                ref="file"
-                @change="uploadFile"
-                accept=".jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|image/*"
-            />
-            <div v-if="pageState === 1" class="text-xs mb-2">
-                <p>ขนาดไฟล์: สูงสุด 1 MB</p>
-                <p>ไฟล์ที่รองรับ: .JPEG, .PNG</p>
-            </div>
-        </div>
+      <Defaultrecord />
     </div>
-    <div class="flex md:mt-10 justify-center">
-        <div v-if="pageState === 1" class="flex flex-col">
-            <compu-button
-                :loading="loading"
-                text="บันทึก"
-                @click="save"
-                class="my-2 border-2 border-[#204E8F] font-semibold text-[#204E8F] py-2 px-6 rounded-lg"
-            />
-            <!-- <button
+    <div v-if="pageState === 1" class="md:w-1/2 w-full md:ml-0 ml-16">
+      <Edit />
+    </div>
+    <div class="w-1/2 flex flex-col items-center">
+      <img
+        class="md:w-40 md:h-40 w-28 h-28 md:mt-8 rounded-full"
+        :src="imagePreview || profileStore.profile?.profileImage || view"
+      />
+      <button
+        v-if="pageState === 1"
+        class="border-[0.05rem] border-black px-2 rounded-lg my-4"
+        @click="$refs.file.click()"
+      >
+        เลือกรูป
+      </button>
+      <input
+        type="file"
+        hidden
+        ref="file"
+        @change="uploadFile"
+        accept=".jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|image/*"
+      />
+      <div v-if="pageState === 1" class="text-xs mb-2">
+        <p>ขนาดไฟล์: สูงสุด 1 MB</p>
+        <p>ไฟล์ที่รองรับ: .JPEG, .PNG</p>
+      </div>
+    </div>
+  </div>
+  <div class="flex md:mt-10 justify-center">
+    <div v-if="pageState === 1" class="flex flex-col">
+      <compu-button
+        :loading="loading"
+        text="บันทึก"
+        @click="save"
+        class="my-2 border-2 border-[#204E8F] font-semibold text-[#204E8F] py-2 px-6 rounded-lg"
+      />
+      <!-- <button
         class="my-2 border-2 border-[#204E8F] font-semibold text-[#204E8F] py-2 px-6 rounded-lg"
         @click="save"
       >
         บันทึก
       </button> -->
-        </div>
-        <button
-            v-if="pageState === 0"
-            class="my-2 border-2 border-[#204E8F] font-semibold text-[#204E8F] py-2 px-6 rounded-lg"
-            @click="edit"
-        >
-            แก้ไข
-        </button>
     </div>
-    <div class="border-b-[0.08rem] border-black mr-12 my-8 w-full"></div>
+    <button
+      v-if="pageState === 0"
+      class="my-2 border-2 border-[#204E8F] font-semibold text-[#204E8F] py-2 px-6 rounded-lg"
+      @click="edit"
+    >
+      แก้ไข
+    </button>
+  </div>
+  <div class="border-b-[0.08rem] border-black mr-12 my-8 w-full"></div>
 </template>
 
 <script>
@@ -81,67 +79,66 @@ import { ref } from "vue";
 import { useProfileStore } from "@/store/profile.store.js";
 
 export default {
-    setup() {
-        const profileStore = useProfileStore();
-        profileStore.fetchMyProfile();
-        return { profileStore };
+  setup() {
+    const profileStore = useProfileStore();
+    profileStore.fetchMyProfile();
+    return { profileStore };
+  },
+  components: {
+    Defaultrecord,
+    Edit,
+    CompuButton,
+  },
+  data() {
+    return {
+      loading: false,
+      view,
+      pageState: 0,
+      profile: "",
+      image: null,
+      imagePreview: null,
+    };
+  },
+  methods: {
+    uploadFile() {
+      this.image = this.$refs.file.files[0];
+      this.imagePreview = URL.createObjectURL(this.image);
     },
-    components: {
-        Defaultrecord,
-        Edit,
-        CompuButton,
+    edit() {
+      this.pageState = 1;
     },
-    data() {
-        return {
-            loading: false,
-            view,
-            pageState: 0,
-            profile: "",
-            image: null,
-            imagePreview: null,
-        };
-    },
-    methods: {
-        uploadFile() {
-            this.image = this.$refs.file.files[0];
-            this.imagePreview = URL.createObjectURL(this.image);
-        },
-        edit() {
-            this.pageState = 1;
-        },
-        save() {
-            if (this.loading) return;
-            this.loading = true;
-            if (this.image) {
-                const formData = new FormData();
-                formData.append("image", this.image);
-                const headers = { "Content-Type": "multipart/form-data" };
+    save() {
+      if (this.loading) return;
+      this.loading = true;
+      if (this.image) {
+        const formData = new FormData();
+        formData.append("image", this.image);
+        const headers = { "Content-Type": "multipart/form-data" };
 
-                axios
-                    .put("/user/edit-profile-image", formData, { headers })
-                    .then(() => {
-                        this.pageState = 0;
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-            }
-            axios
-                .put("/user/edit-user-info", this.profileStore.profile)
-                .then(() => {
-                    this.$toast.success("แก้ไขข้อมูลสำเร็จแล้ว");
-                    this.pageState = 0;
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+        axios
+          .put("/user/edit-profile-image", formData, { headers })
+          .then(() => {
+            this.pageState = 0;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      axios
+        .put("/user/edit-user-info", this.profileStore.profile)
+        .then(() => {
+          this.$toast.success("แก้ไขข้อมูลสำเร็จแล้ว");
+          this.pageState = 0;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
 
-            setTimeout(() => {
-                this.loading = false;
-            }, 3000);
-        },
+      setTimeout(() => {
+        this.loading = false;
+      }, 3000);
     },
-    mounted() {},
+  },
+  mounted() {},
 };
 </script>
-

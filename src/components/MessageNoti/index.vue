3962d9e79f2968fe8c0480f53243006493bb721e
mmayyiisuay <natchasuaysaard@gmail.com>
2023-10-18 12:09:38 +0700

    fix noti

--- src/components/MessageNoti/index.vue
+++ src/components/MessageNoti/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed bg-teal-500 bottom-5 right-5 z-40 rounded w-56 h-20 px-4 py-2 transition-all ease-in-out duration-500"
+    class="fixed text-white bg-primary bottom-5 right-5 z-40 rounded-xl w-56 h-20 px-4 py-2 transition-all ease-in-out duration-500"
     :class="isNoti ? 'right-5' : 'right-[-300px]'"
   >
     <div
